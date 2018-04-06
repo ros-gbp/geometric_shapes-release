f8547e1b05d53057dc2322119349e5df84d4b3ff
Bence Magyar <bence.magyar.robotics@gmail.com>
2018-04-06 20:52:01 +0100

    Rebase from 'upstream'

--- src/shapes.cpp
+++ src/shapes.cpp
@@ -39,68 +39,105 @@
 #include <octomap/octomap.h>
 #include <console_bridge/console.h>
 
-const std::string shapes::Sphere::STRING_NAME = "sphere";
-const std::string shapes::Box::STRING_NAME = "box";
-const std::string shapes::Cylinder::STRING_NAME = "cylinder";
-const std::string shapes::Cone::STRING_NAME = "cone";
-const std::string shapes::Mesh::STRING_NAME = "mesh";
-const std::string shapes::Plane::STRING_NAME = "plane";
-const std::string shapes::OcTree::STRING_NAME = "octree";
+namespace shapes
+{
+const std::string Sphere::STRING_NAME = "sphere";
+const std::string Box::STRING_NAME = "box";
+const std::string Cylinder::STRING_NAME = "cylinder";
+const std::string Cone::STRING_NAME = "cone";
+const std::string Mesh::STRING_NAME = "mesh";
+const std::string Plane::STRING_NAME = "plane";
+const std::string OcTree::STRING_NAME = "octree";
 
-shapes::Shape::Shape()
+std::ostream& operator<<(std::ostream& ss, ShapeType type)
+{
+  switch (type)
+  {
+    case UNKNOWN_SHAPE:
+      ss << "unknown";
+      break;
+    case SPHERE:
+      ss << Sphere::STRING_NAME;
+      break;
+    case CYLINDER:
+      ss << Cylinder::STRING_NAME;
+      break;
+    case CONE:
+      ss << Cone::STRING_NAME;
+      break;
+    case BOX:
+      ss << Box::STRING_NAME;
+      break;
+    case PLANE:
+      ss << Plane::STRING_NAME;
+      break;
+    case MESH:
+      ss << Mesh::STRING_NAME;
+      break;
+    case OCTREE:
+      ss << OcTree::STRING_NAME;
+      break;
+    default:
+      ss << "impossible";
+      break;
+  }
+  return ss;
+}
+
+Shape::Shape()
 {
   type = UNKNOWN_SHAPE;
 }
 
-shapes::Shape::~Shape()
+Shape::~Shape()
 {
 }
 
-shapes::Sphere::Sphere() : Shape()
+Sphere::Sphere() : Shape()
 {
-  type   = SPHERE;
+  type = SPHERE;
   radius = 0.0;
 }
 
-shapes::Sphere::Sphere(double r) : Shape()
+Sphere::Sphere(double r) : Shape()
 {
-  type   = SPHERE;
+  type = SPHERE;
   radius = r;
 }
 
-shapes::Cylinder::Cylinder() : Shape()
+Cylinder::Cylinder() : Shape()
 {
-  type   = CYLINDER;
+  type = CYLINDER;
   length = radius = 0.0;
 }
 
-shapes::Cylinder::Cylinder(double r, double l) : Shape()
+Cylinder::Cylinder(double r, double l) : Shape()
 {
-  type   = CYLINDER;
+  type = CYLINDER;
   length = l;
   radius = r;
 }
 
-shapes::Cone::Cone() : Shape()
+Cone::Cone() : Shape()
 {
-  type   = CONE;
+  type = CONE;
   length = radius = 0.0;
 }
 
-shapes::Cone::Cone(double r, double l) : Shape()
+Cone::Cone(double r, double l) : Shape()
 {
-  type   = CONE;
+  type = CONE;
   length = l;
   radius = r;
 }
 
-shapes::Box::Box() : Shape()
+Box::Box() : Shape()
 {
   type = BOX;
   size[0] = size[1] = size[2] = 0.0;
 }
 
-shapes::Box::Box(double x, double y, double z) : Shape()
+Box::Box(double x, double y, double z) : Shape()
 {
   type = BOX;
   size[0] = x;
@@ -108,7 +145,7 @@ shapes::Box::Box(double x, double y, double z) : Shape()
   size[2] = z;
 }
 
-shapes::Mesh::Mesh() : Shape()
+Mesh::Mesh() : Shape()
 {
   type = MESH;
   vertex_count = 0;
@@ -119,7 +156,7 @@ shapes::Mesh::Mesh() : Shape()
   vertex_normals = NULL;
 }
 
-shapes::Mesh::Mesh(unsigned int v_count, unsigned int t_count) : Shape()
+Mesh::Mesh(unsigned int v_count, unsigned int t_count) : Shape()
 {
   type = MESH;
   vertex_count = v_count;
@@ -130,7 +167,7 @@ shapes::Mesh::Mesh(unsigned int v_count, unsigned int t_count) : Shape()
   vertex_normals = new double[v_count * 3];
 }
 
-shapes::Mesh::~Mesh()
+Mesh::~Mesh()
 {
   if (vertices)
     delete[] vertices;
@@ -142,56 +179,59 @@ shapes::Mesh::~Mesh()
     delete[] vertex_normals;
 }
 
-shapes::Plane::Plane() : Shape()
+Plane::Plane() : Shape()
 {
   type = PLANE;
   a = b = c = d = 0.0;
 }
 
-shapes::Plane::Plane(double pa, double pb, double pc, double pd) : Shape()
+Plane::Plane(double pa, double pb, double pc, double pd) : Shape()
 {
   type = PLANE;
-  a = pa; b = pb; c = pc; d = pd;
+  a = pa;
+  b = pb;
+  c = pc;
+  d = pd;
 }
 
-shapes::OcTree::OcTree() : Shape()
+OcTree::OcTree() : Shape()
 {
   type = OCTREE;
 }
 
-shapes::OcTree::OcTree(const std::shared_ptr<const octomap::OcTree> &t) : octree(t)
+OcTree::OcTree(const std::shared_ptr<const octomap::OcTree>& t) : octree(t)
 {
   type = OCTREE;
 }
 
-shapes::Shape* shapes::Sphere::clone() const
+Shape* Sphere::clone() const
 {
   return new Sphere(radius);
 }
 
-shapes::Shape* shapes::Cylinder::clone() const
+Shape* Cylinder::clone() const
 {
   return new Cylinder(radius, length);
 }
 
-shapes::Shape* shapes::Cone::clone() const
+Shape* Cone::clone() const
 {
   return new Cone(radius, length);
 }
 
-shapes::Shape* shapes::Box::clone() const
+Shape* Box::clone() const
 {
   return new Box(size[0], size[1], size[2]);
 }
 
-shapes::Shape* shapes::Mesh::clone() const
+Shape* Mesh::clone() const
 {
-  Mesh *dest = new Mesh(vertex_count, triangle_count);
+  Mesh* dest = new Mesh(vertex_count, triangle_count);
   unsigned int n = 3 * vertex_count;
-  for (unsigned int i = 0 ; i < n ; ++i)
+  for (unsigned int i = 0; i < n; ++i)
     dest->vertices[i] = vertices[i];
   if (vertex_normals)
-    for (unsigned int i = 0 ; i < n ; ++i)
+    for (unsigned int i = 0; i < n; ++i)
       dest->vertex_normals[i] = vertex_normals[i];
   else
   {
@@ -199,10 +239,10 @@ shapes::Shape* shapes::Mesh::clone() const
     dest->vertex_normals = NULL;
   }
   n = 3 * triangle_count;
-  for (unsigned int i = 0 ; i < n ; ++i)
+  for (unsigned int i = 0; i < n; ++i)
     dest->triangles[i] = triangles[i];
   if (triangle_normals)
-    for (unsigned int i = 0 ; i < n ; ++i)
+    for (unsigned int i = 0; i < n; ++i)
       dest->triangle_normals[i] = triangle_normals[i];
   else
   {
@@ -212,54 +252,54 @@ shapes::Shape* shapes::Mesh::clone() const
   return dest;
 }
 
-shapes::Shape* shapes::Plane::clone() const
+Shape* Plane::clone() const
 {
   return new Plane(a, b, c, d);
 }
 
-shapes::Shape* shapes::OcTree::clone() const
+Shape* OcTree::clone() const
 {
   return new OcTree(octree);
 }
 
-void shapes::OcTree::scaleAndPadd(double scale, double padd)
+void OcTree::scaleAndPadd(double scale, double padd)
 {
-  logWarn("OcTrees cannot be scaled or padded");
+  CONSOLE_BRIDGE_logWarn("OcTrees cannot be scaled or padded");
 }
 
-void shapes::Plane::scaleAndPadd(double scale, double padding)
+void Plane::scaleAndPadd(double scale, double padding)
 {
-  logWarn("Planes cannot be scaled or padded");
+  CONSOLE_BRIDGE_logWarn("Planes cannot be scaled or padded");
 }
 
-void shapes::Shape::scale(double scale)
+void Shape::scale(double scale)
 {
   scaleAndPadd(scale, 0.0);
 }
 
-void shapes::Shape::padd(double padding)
+void Shape::padd(double padding)
 {
   scaleAndPadd(1.0, padding);
 }
 
-void shapes::Sphere::scaleAndPadd(double scale, double padding)
+void Sphere::scaleAndPadd(double scale, double padding)
 {
   radius = radius * scale + padding;
 }
 
-void shapes::Cylinder::scaleAndPadd(double scale, double padding)
-{
-  radius = radius * scale + padding;
-  length = length * scale + 2.0 * padding;
-}
-
-void shapes::Cone::scaleAndPadd(double scale, double padding)
+void Cylinder::scaleAndPadd(double scale, double padding)
 {
   radius = radius * scale + padding;
   length = length * scale + 2.0 * padding;
 }
 
-void shapes::Box::scaleAndPadd(double scale, double padding)
+void Cone::scaleAndPadd(double scale, double padding)
+{
+  radius = radius * scale + padding;
+  length = length * scale + 2.0 * padding;
+}
+
+void Box::scaleAndPadd(double scale, double padding)
 {
   double p2 = padding * 2.0;
   size[0] = size[0] * scale + p2;
@@ -267,11 +307,11 @@ void shapes::Box::scaleAndPadd(double scale, double padding)
   size[2] = size[2] * scale + p2;
 }
 
-void shapes::Mesh::scaleAndPadd(double scale, double padding)
+void Mesh::scaleAndPadd(double scale, double padding)
 {
   // find the center of the mesh
   double sx = 0.0, sy = 0.0, sz = 0.0;
-  for (unsigned int i = 0 ; i < vertex_count ; ++i)
+  for (unsigned int i = 0; i < vertex_count; ++i)
   {
     unsigned int i3 = i * 3;
     sx += vertices[i3];
@@ -283,7 +323,7 @@ void shapes::Mesh::scaleAndPadd(double scale, double padding)
   sz /= (double)vertex_count;
 
   // scale the mesh
-  for (unsigned int i = 0 ; i < vertex_count ; ++i)
+  for (unsigned int i = 0; i < vertex_count; ++i)
   {
     unsigned int i3 = i * 3;
 
@@ -296,16 +336,16 @@ void shapes::Mesh::scaleAndPadd(double scale, double padding)
     double norm = sqrt(dx * dx + dy * dy + dz * dz);
     if (norm > 1e-6)
     {
-      double fact = scale + padding/norm;
+      double fact = scale + padding / norm;
       vertices[i3] = sx + dx * fact;
       vertices[i3 + 1] = sy + dy * fact;
       vertices[i3 + 2] = sz + dz * fact;
     }
     else
     {
-      double ndx = ((dx > 0) ? dx+padding : dx-padding);
-      double ndy = ((dy > 0) ? dy+padding : dy-padding);
-      double ndz = ((dz > 0) ? dz+padding : dz-padding);
+      double ndx = ((dx > 0) ? dx + padding : dx - padding);
+      double ndy = ((dy > 0) ? dy + padding : dy - padding);
+      double ndz = ((dz > 0) ? dz + padding : dz - padding);
       vertices[i3] = sx + ndx;
       vertices[i3 + 1] = sy + ndy;
       vertices[i3 + 2] = sz + ndz;
@@ -313,42 +353,42 @@ void shapes::Mesh::scaleAndPadd(double scale, double padding)
   }
 }
 
-void shapes::Shape::print(std::ostream &out) const
+void Shape::print(std::ostream& out) const
 {
   out << this << std::endl;
 }
 
-void shapes::Sphere::print(std::ostream &out) const
+void Sphere::print(std::ostream& out) const
 {
   out << "Sphere[radius=" << radius << "]" << std::endl;
 }
 
-void shapes::Cylinder::print(std::ostream &out) const
+void Cylinder::print(std::ostream& out) const
 {
   out << "Cylinder[radius=" << radius << ", length=" << length << "]" << std::endl;
 }
 
-void shapes::Cone::print(std::ostream &out) const
+void Cone::print(std::ostream& out) const
 {
   out << "Cone[radius=" << radius << ", length=" << length << "]" << std::endl;
 }
 
-void shapes::Box::print(std::ostream &out) const
+void Box::print(std::ostream& out) const
 {
   out << "Box[x=length=" << size[0] << ", y=width=" << size[1] << "z=height=" << size[2] << "]" << std::endl;
 }
 
-void shapes::Mesh::print(std::ostream &out) const
+void Mesh::print(std::ostream& out) const
 {
   out << "Mesh[vertices=" << vertex_count << ", triangles=" << triangle_count << "]" << std::endl;
 }
 
-void shapes::Plane::print(std::ostream &out) const
+void Plane::print(std::ostream& out) const
 {
   out << "Plane[a=" << a << ", b=" << b << ", c=" << c << ", d=" << d << "]" << std::endl;
 }
 
-void shapes::OcTree::print(std::ostream &out) const
+void OcTree::print(std::ostream& out) const
 {
   if (octree)
   {
@@ -363,28 +403,28 @@ void shapes::OcTree::print(std::ostream &out) const
     out << "OcTree[NULL]" << std::endl;
 }
 
-bool shapes::Shape::isFixed() const
+bool Shape::isFixed() const
 {
   return false;
 }
 
-bool shapes::OcTree::isFixed() const
+bool OcTree::isFixed() const
 {
   return true;
 }
 
-bool shapes::Plane::isFixed() const
+bool Plane::isFixed() const
 {
   return true;
 }
 
-void shapes::Mesh::computeTriangleNormals()
+void Mesh::computeTriangleNormals()
 {
   if (triangle_count && !triangle_normals)
     triangle_normals = new double[triangle_count * 3];
 
   // compute normals
-  for (unsigned int i = 0 ; i < triangle_count ; ++i)
+  for (unsigned int i = 0; i < triangle_count; ++i)
   {
     unsigned int i3 = i * 3;
     Eigen::Vector3d s1(vertices[triangles[i3] * 3] - vertices[triangles[i3 + 1] * 3],
@@ -395,13 +435,13 @@ void shapes::Mesh::computeTriangleNormals()
                        vertices[triangles[i3 + 1] * 3 + 2] - vertices[triangles[i3 + 2] * 3 + 2]);
     Eigen::Vector3d normal = s1.cross(s2);
     normal.normalize();
-    triangle_normals[i3    ] = normal.x();
+    triangle_normals[i3] = normal.x();
     triangle_normals[i3 + 1] = normal.y();
     triangle_normals[i3 + 2] = normal.z();
   }
 }
 
-void shapes::Mesh::computeVertexNormals()
+void Mesh::computeVertexNormals()
 {
   if (!triangle_normals)
     computeTriangleNormals();
@@ -415,25 +455,25 @@ void shapes::Mesh::computeVertexNormals()
     unsigned int tIdx3_1 = tIdx3 + 1;
     unsigned int tIdx3_2 = tIdx3 + 2;
 
-    unsigned int v1 = triangles [tIdx3];
-    unsigned int v2 = triangles [tIdx3_1];
-    unsigned int v3 = triangles [tIdx3_2];
+    unsigned int v1 = triangles[tIdx3];
+    unsigned int v2 = triangles[tIdx3_1];
+    unsigned int v3 = triangles[tIdx3_2];
 
-    avg_normals[v1][0] += triangle_normals [tIdx3];
-    avg_normals[v1][1] += triangle_normals [tIdx3_1];
-    avg_normals[v1][2] += triangle_normals [tIdx3_2];
+    avg_normals[v1][0] += triangle_normals[tIdx3];
+    avg_normals[v1][1] += triangle_normals[tIdx3_1];
+    avg_normals[v1][2] += triangle_normals[tIdx3_2];
 
-    avg_normals[v2][0] += triangle_normals [tIdx3];
-    avg_normals[v2][1] += triangle_normals [tIdx3_1];
-    avg_normals[v2][2] += triangle_normals [tIdx3_2];
+    avg_normals[v2][0] += triangle_normals[tIdx3];
+    avg_normals[v2][1] += triangle_normals[tIdx3_1];
+    avg_normals[v2][2] += triangle_normals[tIdx3_2];
 
-    avg_normals[v3][0] += triangle_normals [tIdx3];
-    avg_normals[v3][1] += triangle_normals [tIdx3_1];
-    avg_normals[v3][2] += triangle_normals [tIdx3_2];
+    avg_normals[v3][0] += triangle_normals[tIdx3];
+    avg_normals[v3][1] += triangle_normals[tIdx3_1];
+    avg_normals[v3][2] += triangle_normals[tIdx3_2];
   }
-  for (std::size_t i = 0 ; i < avg_normals.size() ; ++i)
+  for (std::size_t i = 0; i < avg_normals.size(); ++i)
   {
-    if (avg_normals[i].squaredNorm () > 0.0)
+    if (avg_normals[i].squaredNorm() > 0.0)
       avg_normals[i].normalize();
     unsigned int i3 = i * 3;
     vertex_normals[i3] = avg_normals[i][0];
@@ -442,7 +482,7 @@ void shapes::Mesh::computeVertexNormals()
   }
 }
 
-void shapes::Mesh::mergeVertices(double threshold)
+void Mesh::mergeVertices(double threshold)
 {
   const double thresholdSQR = threshold * threshold;
 
@@ -450,7 +490,7 @@ void shapes::Mesh::mergeVertices(double threshold)
   EigenSTL::vector_Vector3d orig_vertices(vertex_count);
   EigenSTL::vector_Vector3d compressed_vertices;
 
-  for (unsigned int vIdx = 0; vIdx < vertex_count ; ++vIdx)
+  for (unsigned int vIdx = 0; vIdx < vertex_count; ++vIdx)
   {
     orig_vertices[vIdx][0] = vertices[3 * vIdx];
     orig_vertices[vIdx][1] = vertices[3 * vIdx + 1];
@@ -466,7 +506,7 @@ void shapes::Mesh::mergeVertices(double threshold)
     vertex_map[vIdx1] = compressed_vertices.size();
     compressed_vertices.push_back(orig_vertices[vIdx1]);
 
-    for (unsigned int vIdx2 = vIdx1 + 1 ; vIdx2 < vertex_count ; ++vIdx2)
+    for (unsigned int vIdx2 = vIdx1 + 1; vIdx2 < vertex_count; ++vIdx2)
     {
       double distanceSQR = (orig_vertices[vIdx1] - orig_vertices[vIdx2]).squaredNorm();
       if (distanceSQR <= thresholdSQR)
@@ -481,16 +521,16 @@ void shapes::Mesh::mergeVertices(double threshold)
   for (unsigned int tIdx = 0; tIdx < triangle_count; ++tIdx)
   {
     unsigned int i3 = 3 * tIdx;
-    triangles[i3] =  vertex_map[triangles [i3]];
-    triangles[i3 + 1] = vertex_map[triangles [i3 + 1]];
-    triangles[i3 + 2] = vertex_map[triangles [i3 + 2]];
+    triangles[i3] = vertex_map[triangles[i3]];
+    triangles[i3 + 1] = vertex_map[triangles[i3 + 1]];
+    triangles[i3 + 2] = vertex_map[triangles[i3 + 2]];
   }
 
   vertex_count = compressed_vertices.size();
   delete[] vertices;
   vertices = new double[vertex_count * 3];
 
-  for (unsigned int vIdx = 0; vIdx < vertex_count ; ++vIdx)
+  for (unsigned int vIdx = 0; vIdx < vertex_count; ++vIdx)
   {
     unsigned int i3 = 3 * vIdx;
     vertices[i3] = compressed_vertices[vIdx][0];
@@ -503,3 +543,5 @@ void shapes::Mesh::mergeVertices(double threshold)
   if (vertex_normals)
     computeVertexNormals();
 }
+
+} /* namespace shapes */
